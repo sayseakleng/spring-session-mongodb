80e84ebb215c684b1b941899db39106e40dc1d83
sayseakleng <sayseakleng@gmail.com>
2016-06-09 15:45:04 +0700

    Allows spring security to auto-detect login

--- app/src/main/java/com/mcnc/demo/SecurityController.java
+++ app/src/main/java/com/mcnc/demo/SecurityController.java
@@ -1,5 +1,8 @@
 package com.mcnc.demo;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +11,18 @@ public class SecurityController {
     
     @RequestMapping ("/index")
     public String login(){
-    	return "login";
+    	
+    	if(isAuthenticated()) {
+    		return "redirect:/";
+    	}
+    	else {
+    		return "login";
+    	}
     }
  
     
+    private boolean isAuthenticated() {
+    	Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    	return !(authentication instanceof AnonymousAuthenticationToken);
+    }
 }
